6f1cd053cfa4ea6d6789b926befc73b0828671a5
q0952 <97038029+q0952@users.noreply.github.com>
2022-01-10 11:39:53 +0800

    Add files via upload

---
+++ Ackermann.py
@@ -0,0 +1,12 @@
+def ackermann(m,n):
+    if m==0:
+        print(n+1)
+        return n+1
+    elif m>0 and n==0:
+        print("ackermann(",m-1,",",1,")")
+        return ackermann(m-1,1)
+    elif m>0 and n>0:
+        print(("ackermann(",m-1,",","ackermann(",m-1,",",1,")"),")")
+        return ackermann(m-1, ackermann(m, n-1))
+
+print(ackermann(3,4))
